1a0da94661d46c8a108fb8c5ca79aca6b9635c14
Kaushik, Deepali <deepali.kaushik@yahoo.com>
2015-08-27 14:23:00 -0400

    Aug-15 Update

--- app/src/main/java/edu/nyu/scps/deepali/playvideo/AndroidVideoPlayer.java
+++ app/src/main/java/edu/nyu/scps/deepali/playvideo/AndroidVideoPlayer.java
@@ -2,66 +2,67 @@ package edu.nyu.scps.deepali.playvideo;
 /**
  * Created by 205263 on 8/22/15.
  */
+
 import android.app.Activity;
 import android.graphics.PixelFormat;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 //Implement SurfaceHolder interface to Play video
 //Implement this interface to receive information about changes to the surface
-public class AndroidVideoPlayer extends Activity implements SurfaceHolder.Callback{
+public class AndroidVideoPlayer extends Activity implements SurfaceHolder.Callback {
 
-    private MediaPlayer mediaPlayer;
-    SurfaceView surfaceView;
-    SurfaceHolder surfaceHolder;
-    boolean pausing = false;;
-
-
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonPlayVideo = (Button)findViewById(R.id.playvideoplayer);
+        Button buttonPlayVideo = (Button) findViewById(R.id.playvideoplayer);
 
         getWindow().setFormat(PixelFormat.UNKNOWN);
 
         //Displays a video file.
-        VideoView mVideoView = (VideoView)findViewById(R.id.videoview);
+        VideoView mVideoView = (VideoView) findViewById(R.id.videoview);
 
-
-        String uriPath = "android.resource://com.android.AndroidVideoPlayer/"+R.raw.k;
+        String uriPath = "android.resource://" + getPackageName() + "/" + +R.raw.k;
         Uri uri = Uri.parse(uriPath);
         mVideoView.setVideoURI(uri);
         mVideoView.requestFocus();
         mVideoView.start();
 
+        //Video controls
+        mVideoView.setVideoURI(uri);
+        mVideoView.setMediaController(new MediaController(this)); //the controls for the VideoView
+        mVideoView.requestFocus();
 
 
-        buttonPlayVideo.setOnClickListener(new Button.OnClickListener(){
+        buttonPlayVideo.setOnClickListener(new Button.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                // VideoView refference see main.xml
-                VideoView mVideoView = (VideoView)findViewById(R.id.videoview);
+                // VideoView reference see main.xml
+                VideoView mVideoView = (VideoView) findViewById(R.id.videoview);
 
-                String uriPath = "android.resource://com.android.AndroidVideoPlayer/"+R.raw.k;
-
+                String uriPath = "android.resource://" + getPackageName() + "/" + +R.raw.k;
                 Uri uri = Uri.parse(uriPath);
                 mVideoView.setVideoURI(uri);
                 mVideoView.requestFocus();
                 mVideoView.start();
 
 
-            }});
+            }
+        });
+
+
     }
 
     @Override
